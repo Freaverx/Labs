edc4f20e0e336419e01da52a5e18dd0e704c7ecd
Freaverx <deimos.sergei@yandex.ru>
2023-12-24 16:16:52 +0500

    LR4

--- src/LR4/example8.java
+++ src/LR4/example8.java
@@ -3,106 +3,106 @@ package LR4;
 import java.util.Scanner;
 public class example8 {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            Scanner scanner = new Scanner(System.in);
-
-
-
-            System.out.println("Введите текст для шифрования:");
-
-            String text = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
 
 
 
-            System.out.println("Введите ключ:");
+        System.out.println("Введите текст для шифрования:");
 
-            int key = scanner.nextInt();
-
-
-
-            String encryptedText = encrypt(text, key);
-
-            System.out.println("Текст после преобразования: " + encryptedText);
+        String text = scanner.nextLine();
 
 
 
-            System.out.println("Выполнить обратное преобразование? (y/n)");
+        System.out.println("Введите ключ:");
 
-            char choice = scanner.next().charAt(0);
-
-            if (choice == 'y') {
-
-                String decryptedText = decrypt(encryptedText, key);
-
-                System.out.println("Текст после обратного преобразования: " + decryptedText);
-
-            } else {
-
-                System.out.println("До свидания!");
-
-            }
-
-        }
+        int key = scanner.nextInt();
 
 
 
-        public static String encrypt(String text, int key) {
+        String encryptedText = encrypt(text, key);
 
-            StringBuilder result = new StringBuilder();
-
-
-
-            for (char c : text.toCharArray()) {
-
-                if (Character.isLetter(c)) {
-
-                    char encryptedChar = (char) (((c - 'a') + key) % 26 + 'a');
-
-                    result.append(encryptedChar);
-
-                } else {
-
-                    result.append(c);
-
-                }
-
-            }
+        System.out.println("Текст после преобразования: " + encryptedText);
 
 
 
-            return result.toString();
+        System.out.println("Выполнить обратное преобразование? (y/n)");
 
-        }
+        char choice = scanner.next().charAt(0);
 
+        if (choice == 'y') {
 
+            String decryptedText = decrypt(encryptedText, key);
 
-        public static String decrypt(String text, int key) {
+            System.out.println("Текст после обратного преобразования: " + decryptedText);
 
-            StringBuilder result = new StringBuilder();
+        } else {
 
-
-
-            for (char c : text.toCharArray()) {
-
-                if (Character.isLetter(c)) {
-
-                    char decryptedChar = (char) (((c - 'a') - key + 26) % 26 + 'a');
-
-                    result.append(decryptedChar);
-
-                } else {
-
-                    result.append(c);
-
-                }
-
-            }
-
-
-
-            return result.toString();
+            System.out.println("До свидания!");
 
         }
 
     }
+
+
+
+    public static String encrypt(String text, int key) {
+
+        StringBuilder result = new StringBuilder();
+
+
+
+        for (char c : text.toCharArray()) {
+
+            if (Character.isLetter(c)) {
+
+                char encryptedChar = (char) (((c - 'a') + key) % 26 + 'a');
+
+                result.append(encryptedChar);
+
+            } else {
+
+                result.append(c);
+
+            }
+
+        }
+
+
+
+        return result.toString();
+
+    }
+
+
+
+    public static String decrypt(String text, int key) {
+
+        StringBuilder result = new StringBuilder();
+
+
+
+        for (char c : text.toCharArray()) {
+
+            if (Character.isLetter(c)) {
+
+                char decryptedChar = (char) (((c - 'a') - key + 26) % 26 + 'a');
+
+                result.append(decryptedChar);
+
+            } else {
+
+                result.append(c);
+
+            }
+
+        }
+
+
+
+        return result.toString();
+
+    }
+
+}
